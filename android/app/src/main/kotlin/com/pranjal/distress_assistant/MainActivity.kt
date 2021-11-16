dd3e75573ceb8095ee1b531d9eede576b5bcd043
Pranjal <pranjaljsl2910@gmail.com>
2021-11-16 19:16:39 +0530

    heading name on login

--- android/app/src/main/kotlin/com/pranjal/distress_assistant/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.pranjal.distress_assistant
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:distress_assistant/loginPage.dart';
 
+
 User user;
 var a=7;
 void main() {
